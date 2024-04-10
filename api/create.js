8321ea588ab4ac5d8ca58a9a1c702baf00aa07a1
Mohammad Limon <mlimonbd@gmail.com>
2024-04-10 10:59:13 +0600

    change equal sign condition to less strict

--- api/create.js
+++ api/create.js
@@ -373,7 +373,7 @@ function findInArray(needle, array) {
 // Function to check if product exists in the allow_products array
 function checkProductExists(product_id, custom_logo) {
     for (let i = 0; i < custom_logo.allow_products.length; i++) {
-      if (custom_logo.allow_products[i] === product_id) {
+      if (custom_logo.allow_products[i] == product_id) {
         return true;
       }
     }
